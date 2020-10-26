ae65af7f4bfa622e215f32c96f1dfa999e396f26
Jennifer Loecker <>
2020-10-25 19:23:04 -0500

    Added Play, Pause, and Seek buttons on the JS view

--- app/VideoView.js
+++ app/VideoView.js
@@ -1,3 +1,21 @@
-import {requireNativeComponent} from 'react-native';
+import React from 'react';
+import {requireNativeComponent, View, Button} from 'react-native';
 
-module.exports = requireNativeComponent('RNTVideo');
+export default class VideoView extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <RNTVideo {...this.props} />
+        <View style={{flex: 1, justifyContent: 'flex-end', marginBottom: 36}}>
+          <Button title="Play" />
+          <Button title="Pause" />
+          <Button title="Seek Forward" />
+          <Button title="Seek Backward" />
+        </View>
+      </View>
+    );
+  }
+}
+var RNTVideo = requireNativeComponent('RNTVideo', VideoView);
+
+module.exports = VideoView;
